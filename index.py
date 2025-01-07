c26b769126c7fc7244dbce49dfa0747388c12f1f
Pedro Edi dos Santos Galvão <pedroedi12345678g@gmail.com>
2025-01-07 20:25:42 -0300

    Update index.py

--- index.py
+++ index.py
@@ -8,21 +8,14 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
 async def carregar_cogs():
     
-    await bot.load_extension("manager")
 
     for file in os.listdir("Commands"):
         if file.endswith(".py"):
             cog = file[:-3]
             await  bot.load_extension(f'Commands.{cog}')
 
-    for file in os.listdir("Tasks"):
-        if file.endswith(".py"):
-            cog = file[:-3]
-            await  bot.load_extension(f'Tasks.{cog}')
-      
       
 @bot.event
 async def on_ready():
