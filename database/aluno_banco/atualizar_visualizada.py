13d9efa521dee873310f1b6f893b1e64cc55f2a7
Pedro Edi dos Santos Galvão <pedroedi12345678g@gmail.com>
2025-12-01 10:07:22 -0300

    Add files via upload

---
+++ database/aluno_banco/atualizar_visualizada.py
@@ -0,0 +1,26 @@
+from database.database import get_connection
+
+def atualizar_visualizada(nome_discord, titulo):
+    
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    
+    # Obter o ID do aluno
+    cursor.execute("SELECT id FROM aluno WHERE nome_discord = ?", (nome_discord,))
+    aluno = cursor.fetchone()
+
+    if aluno is None:
+        conn.close()
+        return False
+
+    aluno_id = aluno[0]
+
+  
+    cursor.execute('''UPDATE duvidas 
+                    SET visualizada = ? 
+                    WHERE aluno_id = ? AND titulo = ?''', 
+                (True, aluno_id, titulo))
+
+    conn.commit()
+    conn.close()
