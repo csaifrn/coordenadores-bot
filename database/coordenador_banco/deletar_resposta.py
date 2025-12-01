2b840b3d3f839a17a46751e6a1ce0c0557a5e574
davidmtg <124364476+davidmtg@users.noreply.github.com>
2025-12-01 10:20:21 -0300

    Create deletar_resposta.py

---
+++ database/coordenador_banco/deletar_resposta.py
@@ -0,0 +1,26 @@
+from database.database import get_connection 
+
+def deletar_resposta(nome_discord, titulo):
+    
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    # Obter o ID do aluno
+    cursor.execute("SELECT id FROM aluno WHERE nome_discord = ?", (nome_discord,))
+    aluno = cursor.fetchone()
+
+    if aluno is None:
+        conn.close()
+        return False  # Retorna False caso o aluno não exista
+
+    aluno_id = aluno[0]
+
+    # Atualizar os campos resposta e timestamp_resposta para NULL
+    cursor.execute('''
+        UPDATE duvidas 
+        SET resposta = NULL, timestamp_resposta = NULL
+        WHERE aluno_id = ? AND titulo = ?
+    ''', (aluno_id, titulo))
+
+    conn.commit()
+    conn.close()
